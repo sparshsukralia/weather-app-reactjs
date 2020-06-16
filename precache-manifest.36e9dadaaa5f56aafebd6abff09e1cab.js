adc87b2dc7f12d756d303e3583f581acf6bab883
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-06-17 00:24:35 +0545

    Updates

---
+++ precache-manifest.36e9dadaaa5f56aafebd6abff09e1cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df056fbbe036731bdba14eb136710632",
+    "url": "/weather-app-deploy/index.html"
+  },
+  {
+    "revision": "d7e205d7f8547acedb28",
+    "url": "/weather-app-deploy/static/css/main.aef713b7.chunk.css"
+  },
+  {
+    "revision": "192f7153a9658b18af3e",
+    "url": "/weather-app-deploy/static/js/2.330f580b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-deploy/static/js/2.330f580b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e205d7f8547acedb28",
+    "url": "/weather-app-deploy/static/js/main.e3eca9af.chunk.js"
+  },
+  {
+    "revision": "4848d1a5db7d8b337564",
+    "url": "/weather-app-deploy/static/js/runtime-main.f4ec0ed4.js"
+  }
+]);
